f219a89b7077b11fad270367d932a74456fde1c2
Hamid Shafi'ee <hmd.net@gmail.com>
2018-07-17 20:08:16 +0430

    Update demo.js

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -579,7 +579,7 @@ demo = {
         }
         console.log('latlang');
         console.log(latlang);
-        console.log(latlang[0].lat());
+        console.log(latlang[0].lat);
         var markerCluster = new MarkerClusterer(map, markers,
             { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
